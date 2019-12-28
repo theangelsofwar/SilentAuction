10e225de66f37405b90d5449a16c2f737adf4edb
angiechangpagne@gmail.com <angiechangpagne@gmail.com>
2019-12-27 23:09:14 -0800

    axios

--- boilerman/src/components/Home.js
+++ boilerman/src/components/Home.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">
-         Home 
-      </h4>
-      <p> Current Live Bids: </p>
-    </div>
-  )
+class Home extends Component {
+  state = {
+    posts:[ ]
+  }
+  componentDidMount(){
+    axios.get('https://jsonplaceholder.typicode.com/posts/')
+    .then(res=> {
+      console.log("res in line 8 of Home is ",res);
+      this.setState({
+        posts: res.data.slice(0,10)
+      });
+    })
+  }
+  render(){
+    const { posts } = this.state;
+    const postList = posts.length ?  
+      (posts.map(post => {
+        return (
+          <div className="post card" key={post.id}> 
+            <div className="card-content">
+              <span className="card-title">{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+      )
+    })
+    ) : (
+      <div className="center"> No Bids Yet</div>
+    );
+    return (
+      <div className="container">
+        <h4 className="center">
+          Home 
+        </h4>
+        <p> Current Live Bids: </p>
+        {postList}
+      </div>
+    )
+  }
 }
 
 export default Home;
